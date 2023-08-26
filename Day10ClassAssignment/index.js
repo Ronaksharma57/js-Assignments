38bba6d3da38dfcdb026412d6e4465b7fbbff805
RONAK SHARMA <78947568+Ronaksharma57@users.noreply.github.com>
2023-08-26 11:43:25 +0530

    Create index.js

---
+++ Day10ClassAssignment/index.js
@@ -0,0 +1,40 @@
+    var Color = [
+  "red",
+  "green",
+  "cyan",
+  "black",
+  "voilet",
+  "blue",
+  "yellow",
+  "purple",
+  "orange",
+  "lightgreen",
+  "lemon",
+];
+var index = 0 ;
+const circle = document.getElementById("circle");
+        const shape = document.querySelector(".shape");
+        const Changecolor = document.querySelector("#color");
+        const changeShape = document.querySelector("#changeShape");
+        color.addEventListener("click",()=>{
+          
+        if (index === Color.length) {
+        index = 0;
+  }
+        circle.style.backgroundColor = Color[index];
+        index++;
+        })
+        var Tringle = false;
+        changeShape.addEventListener("click",()=>{
+            if(!Tringle){
+                var Shape = document.querySelector(".shape");
+                Shape.className = "triangle";
+                Tringle = true;
+            }
+            else{
+                var shape = document.querySelector(".triangle");
+                shape.className = "shape";
+                Tringle = false;
+            }
+        })
+      
